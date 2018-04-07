ffee6e7ec69754ed8b8fa03a7adbbd3a2b4f60d1
Adam Bezak <bezoadam95@gmail.com>
2018-04-07 17:56:22 +0200

    refactoring part 1

--- shellXcode/shell/shell/main.c
+++ shellXcode/shell/shell/main.c
@@ -8,212 +8,10 @@
 
 #include "shell.h"
 
-struct Monitor monitorGlobal;        /* mutex */
+/* Globalne premenne */
+struct Monitor monitorGlobal;
 struct Buffer bufferGlobal;
-bool end = false;    /* koncovy stav */
-
-void printError(int err) {
-    fprintf(stderr, errors[err]);
-}
-
-/*
- * ==== handler pro sig int
- * */
-void sigIntHandler(int sig){
-
-}
-
-/*
- * ==== handler pro sig chld
- * */
-void sigChldHandler(int sig, siginfo_t *pid, void *contxt){
-
-    waitpid(pid->si_pid, NULL, 0);
-    if(DEBUG){
-        printf("dokonceno\n");
-    }
-}
-
-int initSigHandlers(struct sigaction *sigInt, struct sigaction *sigChld) {
-
-    sigInt->sa_handler = sigIntHandler;
-    sigInt->sa_flags = SA_RESTART;
-    sigemptyset(&sigInt->sa_mask);
-
-    /* set sig action - int*/
-    if(sigaction(SIGINT, sigInt, NULL) == -1){
-        return ERR_SET_SIG_INT;
-    }
-
-    /* handler pro chld */
-    sigChld->sa_sigaction  = sigChldHandler;
-    sigChld->sa_flags    = SA_RESTART | SA_SIGINFO | SA_NOCLDSTOP;
-    sigemptyset(&sigChld->sa_mask);
-
-    /* set sig action - chld */
-    if(sigaction(SIGCHLD, sigChld, NULL) == -1){
-        return ERR_SET_SIG_CHLD;
-    }
-
-    return OK;
-}
-
-/*
- * ==== vstup do kriticke sekce
- * */
-void await(bool reading){
-    pthread_mutex_lock(&(monitorGlobal.mutex));
-    while(reading ? !bufferGlobal.reading : bufferGlobal.reading){
-        pthread_cond_wait(&(monitorGlobal.cond), &(monitorGlobal.mutex));
-    }
-    return;
-}
-
-/*
- * ==== uvolneni kriticke sekce
- * */
-void advance(bool with_signal){
-    /* odblokovani cekajici udalosti + vystup z KS */
-    if(with_signal) pthread_cond_signal(&(monitorGlobal.cond));
-    pthread_mutex_unlock(&(monitorGlobal.mutex));
-    return;
-}
-
-/*
- * ==== vlakno pro spousteni prikazu = shell
- * */
-void *spousteniPrikazu(void *t){
-    while(!end){
-        /* vstup do KS */
-        await(false);
-
-        /* obsluha prikazove radky */
-        if(DEBUG) printf("shell: delka vstupu: %d-%d\n'%s'\n", (int)strlen(bufferGlobal.read_buffer), bufferGlobal.length, bufferGlobal.read_buffer);
-
-        /* ukonceni shellu */
-        if(strcmp("exit", bufferGlobal.read_buffer) == 0){
-            end = true;
-        }
-
-        /* zpracovani prikazu */
-        else if(bufferGlobal.length > 0){
-            printf("%s", bufferGlobal.read_buffer);
-        }
-
-        /* vystup z KS */
-        bufferGlobal.reading = true;
-        advance(true);
-    }
-    pthread_exit(t);
-}
-
-/*
- * ==== inicializace monitoru
- * */
-int initMonitor(struct Monitor *monitor, pthread_t *thread){
-
-    /* inicializace mutexu */
-    if(pthread_mutex_init(&(monitor->mutex), NULL) != 0){
-        return ERR_MUTEX;
-    }
-
-    /* inicializace condu */
-    if(pthread_cond_init(&(monitor->cond), NULL) != 0){
-        pthread_mutex_destroy(&(monitor->mutex));
-        return ERR_COND;
-    }
-
-    /* vytvoreni vlakna pro spousteni prikazu */
-    if(pthread_create(thread, NULL, spousteniPrikazu, NULL) != 0){
-        pthread_cond_destroy(&(monitor->cond));
-        pthread_mutex_destroy(&(monitor->mutex));
-        return ERR_THREAD;
-    }
-
-    return OK;
-}
-
-/*
- * ==== uvolneni pthreads
- * */
-void freeMonitor(struct Monitor *monitor, pthread_t *thread){
-    pthread_join(*thread, NULL);
-    pthread_cond_destroy(&(monitor->cond));
-    pthread_mutex_destroy(&(monitor->mutex));
-    return;
-}
-
-/*
- * ==== cteni ze stdin pomoci fce read
- * */
-int readStdin(){
-    bufferGlobal.length = 0;
-    bufferGlobal.read_buffer[0] = '\0';
-
-    bufferGlobal.length = read(STDIN_FILENO, (void*)bufferGlobal.read_buffer, BUFFER_MAX_LENGTH + 1);
-
-    /* kontrola delky - BUFFER_MAX_LENGTH */
-    if(bufferGlobal.length > BUFFER_MAX_LENGTH && bufferGlobal.read_buffer[BUFFER_MAX_LENGTH] != '\n'){
-        while('\n' != getchar());
-        return ERR_BUFFER_MAX_LENGTH;
-    }
-    else if(bufferGlobal.length > 0){
-        /* nastaveni konce stringu */
-        bufferGlobal.read_buffer[bufferGlobal.length-1] = '\0';
-        bufferGlobal.length = (int)strlen(bufferGlobal.read_buffer);
-    }
-    return OK;
-}
-
-/*
- * ==== vypis promptu
- * */
-void showPrompt(){
-    write(STDOUT_FILENO, (void*)PROMPT, strlen(PROMPT));
-    return;
-}
-
-/*
- * ==== cteni vstupu
- * */
-int readMyInput(){
-    int err;
-    bool with_signal = true;
-
-    while (!end){
-        /* vstup do KS */
-        await(true);
-
-        /* muzume zacit cist dokoncene procesy a zapsat prompt */
-        if(!end){
-
-            /* vypis promptu */
-            showPrompt();
-
-            /* cteni ze stdin */
-            if((err = readStdin()) != OK){
-                bufferGlobal.reading = true;
-                printError(err);
-            }
-            else{
-                bufferGlobal.reading = false;
-            }
-
-            with_signal = true;
-        }
-        /* nastal end - neni co odblokovat */
-        else{
-            bufferGlobal.reading = false;
-            with_signal = false;
-        }
-
-        /* odblokovani cekajici udalosti + vystup z KS */
-        advance(with_signal);
-        if(!with_signal) return OK;
-    }
-
-    return OK;
-}
+bool end = false;
 
 int main(int argc, const char * argv[]) {
 
@@ -225,7 +23,7 @@ int main(int argc, const char * argv[]) {
 
     /* kontrola parametru */
     if(argc != 1){
-        printf("%s", HELP_MSG);
+        printf("%s", HELP);
         return EXIT_FAILURE;
     }
 
@@ -256,3 +54,171 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
+
+
+
+int initSigHandlers(struct sigaction *sigInt, struct sigaction *sigChld) {
+    sigInt->sa_handler = sigIntHandler;
+    sigInt->sa_flags = SA_RESTART;
+    sigemptyset(&sigInt->sa_mask);
+
+    if(sigaction(SIGINT, sigInt, NULL) == -1){
+        return SIG_INT_ERR;
+    }
+
+    sigChld->sa_sigaction  = sigChldHandler;
+    sigChld->sa_flags    = SA_RESTART | SA_SIGINFO | SA_NOCLDSTOP;
+    sigemptyset(&sigChld->sa_mask);
+
+    if(sigaction(SIGCHLD, sigChld, NULL) == -1){
+        return SIG_CHLD_ERR;
+    }
+
+    return OK;
+}
+
+
+void sigIntHandler(int sig) {
+
+}
+
+/*
+ * ==== handler pro sig chld
+ * */
+void sigChldHandler(int sig, siginfo_t *pid, void *contxt) {
+
+    waitpid(pid->si_pid, NULL, 0);
+    if(DEBUG){
+        printf("dokonceno\n");
+    }
+}
+
+int initMonitor(struct Monitor *monitor, pthread_t *thread) {
+
+    /* init mutexu */
+    if(pthread_mutex_init(&(monitor->mutex), NULL) != 0){
+        return MUTEX_ERR;
+    }
+
+    /* init cond */
+    if(pthread_cond_init(&(monitor->cond), NULL) != 0){
+        pthread_mutex_destroy(&(monitor->mutex));
+        return COND_ERR;
+    }
+
+    /* Vytvorenie druheho vlakna na provedeni prikazu */
+    while (pthread_create(thread, NULL, runCommand, NULL)){
+        pthread_cond_destroy(&(monitor->cond));
+        pthread_mutex_destroy(&(monitor->mutex));
+        return THREAD_ERR;
+    }
+
+    return OK;
+}
+
+void *runCommand(void *arg) {
+    while(!end){
+        /* vstup do KS */
+        lockKS(false);
+
+        /* obsluha prikazove radky */
+        if(DEBUG) printf("shell: delka vstupu: %d-%d\n'%s'\n", (int)strlen(bufferGlobal.read_buffer), bufferGlobal.length, bufferGlobal.read_buffer);
+
+        /* ukonceni shellu */
+        if(strcmp("exit", bufferGlobal.read_buffer) == 0){
+            end = true;
+        }
+
+        /* zpracovani prikazu */
+        else if(bufferGlobal.length > 0){
+            printf("%s", bufferGlobal.read_buffer);
+        }
+
+        /* vystup z KS */
+        bufferGlobal.reading = true;
+        unlockKS(true);
+    }
+    pthread_exit(arg);
+}
+
+int readMyInput() {
+    int err;
+    bool with_signal = true;
+
+    while (!end){
+        /* vstup do KS */
+        lockKS(true);
+
+        /* muzume zacit cist dokoncene procesy a zapsat prompt */
+        if(!end){
+
+            /* PROMPT */
+            write(STDOUT_FILENO, (void*)PROMPT, strlen(PROMPT));
+
+            /* cteni ze stdin */
+            if((err = readStdin()) != OK){
+                bufferGlobal.reading = true;
+                printError(err);
+            }
+            else{
+                bufferGlobal.reading = false;
+            }
+
+            with_signal = true;
+        }
+        /* nastal end - neni co odblokovat */
+        else{
+            bufferGlobal.reading = false;
+            with_signal = false;
+        }
+
+        /* odblokovani cekajici udalosti + vystup z KS */
+        unlockKS(with_signal);
+        if(!with_signal) return OK;
+    }
+
+    return OK;
+}
+
+int readStdin() {
+    bufferGlobal.length = 0;
+    bufferGlobal.read_buffer[0] = '\0';
+
+    bufferGlobal.length = read(STDIN_FILENO, (void*)bufferGlobal.read_buffer, BUFFER_MAX_LENGTH + 1);
+
+    /* kontrola delky - BUFFER_MAX_LENGTH */
+    if(bufferGlobal.length > BUFFER_MAX_LENGTH && bufferGlobal.read_buffer[BUFFER_MAX_LENGTH] != '\n'){
+        while('\n' != getchar());
+        return BUFFER_MAX_LENGTH_ERR;
+    }
+    else if(bufferGlobal.length > 0){
+        /* nastaveni konce stringu */
+        bufferGlobal.read_buffer[bufferGlobal.length-1] = '\0';
+        bufferGlobal.length = (int)strlen(bufferGlobal.read_buffer);
+    }
+    return OK;
+}
+
+void lockKS(bool reading) {
+    pthread_mutex_lock(&(monitorGlobal.mutex));
+    while(reading ? !bufferGlobal.reading : bufferGlobal.reading){
+        pthread_cond_wait(&(monitorGlobal.cond), &(monitorGlobal.mutex));
+    }
+}
+
+void unlockKS(bool with_signal) {
+    /* odblokovani cekajici udalosti + vystup z KS */
+    if(with_signal) pthread_cond_signal(&(monitorGlobal.cond));
+    pthread_mutex_unlock(&(monitorGlobal.mutex));
+}
+
+void freeMonitor(struct Monitor *monitor, pthread_t *thread) {
+    pthread_join(*thread, NULL);
+    pthread_cond_destroy(&(monitor->cond));
+    pthread_mutex_destroy(&(monitor->mutex));
+}
+
+void printError(int err) {
+    fprintf(stderr, "%s", errors[err]);
+}
+
--- shellXcode/shell/shell/shell.h
+++ shellXcode/shell/shell/shell.h
@@ -32,33 +32,33 @@
 
 enum {
     OK = 0,
-    ERR_SET_SIG_INT,
-    ERR_SET_SIG_CHLD,
-    ERR_MUTEX,
-    ERR_COND,
-    ERR_THREAD,
-    ERR_MALLOC_ADD_LIST,
-    ERR_BUFFER_MAX_LENGTH,
-    ERR_PARSE_AMPERSAND,
-    ERR_PARSE_SPECIAL_CHARS,
-    ERR_PARSE_FILE,
-    ERR_FORK,
-    ERR_CMD_BG,
-    ERR_INPUT_FILE,
-    ERR_OUTPUT_FILE,
-    ERR_MALLOC,
-    ERR_CMD
+    SIG_INT_ERR,
+    SIG_CHLD_ERR,
+    THREAD_ERR,
+    COND_ERR,
+    MUTEX_ERR,
+    BUFFER_MAX_LENGTH_ERR,
+    ERR_MALLOC_ADD_LIST, //unused
+    ERR_PARSE_AMPERSAND, //unused
+    ERR_PARSE_SPECIAL_CHARS, //unused
+    ERR_PARSE_FILE, //unused
+    ERR_FORK, //unused
+    ERR_CMD_BG, //unused
+    ERR_INPUT_FILE, //unused
+    ERR_OUTPUT_FILE, //unused
+    ERR_MALLOC, //unused
+    ERR_CMD //unused
 };
 
 const char *errors[] = {
-    "",
-    "Chyba sigaction: sig int\n",
-    "Chyba sigaction: sig chld\n",
-    "Chyba inicializace mutexu\n",
-    "Chyba inicializace cond\n",
-    "Chyba pri vytvareni vlakna\n",
+    "OK",
+    "SIG INT ERR\n",
+    "SIG CHLD ERR\n",
+    "Chyba pri vytvarani vlakna.\n",
+    "Chyba pri inicializacii cond.\n",
+    "Chyba pri inicializacii mutexu.\n",
+    "Prilis dlhy vstup.\n",
     "Chyba alokovani dat pri pridavani prvku do seznamu\n",
-    "Chyba: maximalni delka vstupu byla prekrocena\n",
     "Chyba syntaxe: za ampersandem nesmi nasledovat dalsi text bez mezery\n",
     "Chyba syntaxe: specialni znaky &,<,> musi byt v prikazu at na poslednich mistech\n",
     "Chyba syntaxe: vstupni/vystupni soubor byl spatne zadan\n",
@@ -71,19 +71,16 @@ const char *errors[] = {
 };
 
 /* napoveda programu */
-const char *HELP_MSG    = "Program: Shell\n"
-"Autor: Adam Bezak | xbezak01\n\n"
-"Pouziti: ./shell \n"
-"    - spusteni libovolneho program s libovolnymi parametry\n"
-"    - pri spusteni se bere v uvahu promenna PATH (nastavena pred spustenim shellu)\n"
-"    - spusteni na pozadi pomoci &\n"
-"    - presmerovani vstupu na vystup pomoci <a>\n"
-"    - po spusteni shell vypise prompt a bude cekat na vstup pomoci read()\n"
-"    - shell pouziva dve vlakna, prvni pro ziskani vstupu a druhe pro spousteni prikazu"
-"Priklad pouziti: \n"
-"    $ ls -l -a\n"
-"    $ sleep 10m &\n"
-"    $ echo karel >/tmp/test\n"
+const char *HELP =
+"Spustenie programu: ./shell \n"
+"- spustenie lubovolneho programu v lubovolnymi parametramy\n"
+"- pri spusteni sa bere v uvahu premenna PATH (nastavena pred spustenim shellu)\n"
+"- spustenie na pozadi pomocou &\n"
+"- presmerovanie vstupu a vystupu pomocou < a >\n"
+"Priklad pouzitia programu: \n"
+"$ ls -l -a\n"
+"$ sleep 10m &\n"
+"$ echo karel >/tmp/test\n"
 "\n"
 ;
 
@@ -99,3 +96,54 @@ struct Buffer {
     bool reading;
 
 };
+
+/* deklarace funkcii */
+int initSigHandlers(struct sigaction *sigInt, struct sigaction *sigChld);
+
+/*
+ * Sig int handler
+ *
+ */
+void sigIntHandler(int sig);
+
+/*
+ * Sig chld handler
+ *
+ */
+void sigChldHandler(int sig, siginfo_t *pid, void *contxt);
+
+/*
+ * ==== inicializace monitoru
+ * */
+int initMonitor(struct Monitor *monitor, pthread_t *thread);
+
+/*
+ * ==== vlakno pro spousteni prikazu = shell
+ * */
+void *runCommand(void *arg);
+
+/*
+ * ==== cteni vstupu
+ * */
+int readMyInput(void);
+
+/*
+ * ==== cteni ze stdin pomoci fce read
+ * */
+int readStdin(void);
+
+/*
+ * ==== vstup do kriticke sekce
+ * */
+void lockKS(bool reading);
+
+/*
+ * ==== uvolneni kriticke sekce
+ * */
+void unlockKS(bool with_signal);
+
+/*
+ * ==== uvolneni pthreads
+ * */
+void freeMonitor(struct Monitor *monitor, pthread_t *thread);
+void printError(int err);
